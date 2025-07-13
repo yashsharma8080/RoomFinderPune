cda772d6340207f361566d1cd201ef08cbfa1cd7
Yash Sharma <thesharmaji8080@gmail.com>
2025-07-13 16:35:58 +0530

    Add App.jsx to src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import RoomList from "./RoomList";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <RoomList />
+    </div>
+  );
+}
